2e766bfc93e1a7ea85d3edd6d1d6f75c46045f8f
harinduliyanage <harindu.sul@gmail.com>
2023-02-26 15:23:54 +0530

    initial commit

---
+++ src/main/java/com/codecafe/pptbackend/PptBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.pptbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PptBackendApplication.class, args);
+	}
+
+}
